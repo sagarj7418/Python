3be624be21653ad3945515bf90d10763375f4c3c
Sagar <105412762+sagarj7418@users.noreply.github.com>
2022-05-28 23:35:45 +0530

    String methods

---
+++ 01_string_practice.py
@@ -0,0 +1,17 @@
+a='my name is sagar and age is 31'
+print(a)
+
+print(len(a))           # length of string
+'''
+print(type(a))          # type of a is string
+print(a[0])             # string slicing
+print(a[0:5])
+print(a[::-1])          # string reverse
+print(a[0:15:2])        # slicing with skip value
+print(a.capitalize())   # Upper case the first letter in this sentence
+print(a.casefold())     # Make the string lower case
+print(a.center(100, '*')) # will center align the string, using a specified character (space is default) as the fill character.
+print(a.count('a'))     # returns the number of times a specified value appears in the string.
+print(a.startswith('my'))   # returns True if the string starts with the specified value, otherwise False.
+print(a.endswith('ar'))     # returns True if the string ends with the specified value, otherwise False. 
+'''
